5b2dbbdc509db21a10281088cf84ac3f38087f10
Hajibar Jhoneil Fernandez <haji@Hajibars-MacBook-Pro.local>
2020-12-20 19:28:20 +0800

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Nunito"],
+      },
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
